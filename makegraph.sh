d1cb958745e00f4f790bcacdf0a420cb227a5a88
Neo_Chen <chenkolei@gmail.com>
2020-03-15 20:07:09 +0800

    makegraph.sh: use optipng to reduce nodes.png's size

--- makegraph.sh
+++ makegraph.sh
@@ -7,3 +7,11 @@ if which dot 2>&1 > /dev/null ; then
 else
 	echo 'You need to install graphviz first'
 fi
+
+if [ "$1" != 'noopt' ]; then
+	if which optipng 2>&1 > /dev/null ; then
+		optipng -o7 -zm9 -v nodes.png
+	else
+		echo 'optipng is not present, not optimizing compression'
+	fi
+fi
