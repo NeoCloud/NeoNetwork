ba496f543d17e5afc1f5f5220db567aec25d6409
Neo_Chen <chenkolei@gmail.com>
2020-04-21 08:49:08 +0800

    separate homepage

---
+++ homepage-update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -xeu
+
+if ! which markdown 2>&1 > /dev/null; then
+	echo "need markdown"
+	exit
+fi
+
+(
+	cat docs/header.html
+	markdown README.md
+	cat docs/footer.html
+) > docs/index.html
