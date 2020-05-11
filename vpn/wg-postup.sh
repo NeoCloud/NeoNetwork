738230d4ccc9788f4d828d8a614984574dd1c157
Neo_Chen <chenkolei@gmail.com>
2020-05-11 21:14:38 +0800

    VPN: wg-postup.sh: missing dev

--- vpn/wg-postup.sh
+++ vpn/wg-postup.sh
@@ -8,5 +8,5 @@ PEER_IPV6="$5"
 
 ip addr del "$LOCAL_IP" dev "$IF"
 ip addr add "$LOCAL_IP" peer "$PEER_IP" dev "$IF"
-ip addr del "$LOCAL_IPV6"
+ip addr del "$LOCAL_IPV6" dev "$IF"
 ip addr add "$LOCAL_IPV6" peer "$PEER_IPV6" dev "$IF"
