73ac665c31b4c4ef6c92760d53cee15db9b84db2
Septs <github@septs.pw>
2020-07-21 00:40:22 +0800

    update scripts

--- .scripts/push-to-roa.sh
+++ .scripts/push-to-roa.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 IFS=$'\n\t'
 
-pushd roa_dir
 if [ -n "$SSHPRIVKEY" ]; then
 	echo SSHPRIVKEY is not set
 	exit 1
 fi
+
 mkdir -p "$HOME/.ssh"
 echo "$SSHPRIVKEY" | base64 -d >"$HOME/.ssh/id_ed25519"
 chmod 0600 "$HOME/.ssh/id_ed25519"
-ssh -o StrictHostKeyChecking=no git@github.com || true
-git init && git add .
+
+set -x
+
+pushd generated
+git add .
 git config user.name "neonet roa bot"
 git config user.email "bot@github.com"
 git commit -m "Generated at $(TZ='UTC' date +%Y%m%d-%H%M%S.%N)"
