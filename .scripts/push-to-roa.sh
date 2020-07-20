8c1b87eaa42823700d3d6531eecdec5e4bbec427
Septs <github@septs.pw>
2020-07-21 02:03:50 +0800

    Regular update: 2020-07-20T18:03:50Z

--- .scripts/push-to-roa.sh
+++ .scripts/push-to-roa.sh
@@ -13,10 +13,11 @@ chmod 0600 "$HOME/.ssh/id_ed25519"
 
 set -x
 
-pushd generated
+cd generated || exit -1
+
 git remote set-url origin git@github.com:NeoCloud/NeoNetwork-ROA.git
+git config user.name "NeoCloud ROA bot"
+git config user.email "support@neocloud.tw"
 git add .
-git config user.name "neonet roa bot"
-git config user.email "bot@github.com"
-git commit -m "Generated at $(TZ='UTC' date +%Y%m%d-%H%M%S.%N)"
+git commit -m "Generated at $(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 git push
