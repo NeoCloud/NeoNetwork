deeed8e20c95ba985a56b30ff607c84af422e1fd
Septs <github@septs.pw>
2020-07-21 11:30:06 +0800

    Regular update: 2020-07-21T03:30:06Z

--- .scripts/generate-roa.sh
+++ .scripts/generate-roa.sh
@@ -8,6 +8,8 @@ set -x
 export MAX_LEN_4=29
 export MAX_LEN_6=64
 
+mkdir -p generated
+
 .scripts/roa.py -m "$MAX_LEN_4" -M "$MAX_LEN_6" -o generated/roa46_bird2.conf
 .scripts/roa.py -m "$MAX_LEN_4" -M "$MAX_LEN_6" -4 -o generated/roa4_bird2.conf
 .scripts/roa.py -m "$MAX_LEN_4" -M "$MAX_LEN_6" -6 -o generated/roa6_bird2.conf
