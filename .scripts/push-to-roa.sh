4573aeab7338aafdcd14c825cff687c64075d1fd
Septs <github@septs.pw>
2020-07-26 19:30:54 +0800

    Regular update: 2020-07-26T11:30:54Z

--- .scripts/push-to-roa.sh
+++ .scripts/push-to-roa.sh
@@ -2,20 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ -z "$SSHPRIVKEY" ]; then
-	echo SSHPRIVKEY is not set
-	exit 1
-fi
-
-mkdir -p "$HOME/.ssh"
-echo "$SSHPRIVKEY" | base64 -d >"$HOME/.ssh/id_ed25519"
-chmod 0600 "$HOME/.ssh/id_ed25519"
-
 set -x
 
 cd generated || exit 1
 
-git remote set-url origin git@github.com:NeoCloud/NeoNetwork-ROA.git
 git config user.name "NeoCloud ROA bot"
 git config user.email "support@neocloud.tw"
 git add .
