a386fbf0c0421828e4ce2aabd57d780b99a711ac
Neo_Chen <chenkolei@gmail.com>
2023-05-02 23:44:44 +0800

    updated WireGuard config template

--- vpn/wg-postup.sh
+++ vpn/wg-postup.sh
@@ -6,10 +6,8 @@ PEER_IP="$3"
 LOCAL_IPV6="$4"
 PEER_IPV6="$5"
 
-ip addr del "$LOCAL_IP" dev "$IF"
 ip addr add "$LOCAL_IP" peer "$PEER_IP" dev "$IF"
 
 if [ "$#" -ge 5 ]; then
-	ip addr del "$LOCAL_IPV6" dev "$IF"
 	ip addr add "$LOCAL_IPV6" peer "$PEER_IPV6" dev "$IF"
 fi
